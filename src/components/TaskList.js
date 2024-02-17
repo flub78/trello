f4383dc255af0c45b7e3c2ca42df0dc2948b98ea
flub78 <frederic.peignot>
2024-02-17 18:52:37 +0100

    css WIP

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -5,6 +5,14 @@ const createTask = () => {
     console.log('create task');
 }
 
+const validateCreateTask = () => {
+    console.log('validate create task');
+}
+
+const cancelCreateTask = () => {
+    console.log('cancel create task');
+}
+
 const TaskList = ({ list }) => {
 
 
@@ -32,8 +40,16 @@ const TaskList = ({ list }) => {
                 })
             }
 
-            <div className="create-task" onClick={createTask}>+ Ajouter une carte <i className="bi  bi-copy create-task-icon
-"></i></div>
+            <div className="create-task_open" >
+                <textarea className="m-1 p-1 w-100" type="text" placeholder="Saisissez un titre pour cette carte..." />
+                <button className="btn btn-primary m-1" onClick={validateCreateTask}>
+                    Ajouter une carte</button>
+                <i className="bi bi-x-lg m-1" onClick={cancelCreateTask}></i>
+            </div>
+
+            <div className="create-task" onClick={createTask}>
+                + Ajouter une carte <i className="bi  bi-copy create-task-icon"></i>
+            </div>
         </div>
 
     );
