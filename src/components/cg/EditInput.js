f3b93e7b6c5a80d05d7f339e12e155197bd3de31
flub78 <frederic.peignot>
2024-04-13 09:33:30 +0200

    WIP

--- src/components/cg/EditInput.js
+++ src/components/cg/EditInput.js
@@ -1,30 +1,39 @@
 import React from 'react';
 
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+
 const EditInput = ({ descriptor, value, onChange }) => {
+
+    if (descriptor.subtype === 'boolean') {
+        return (
+            <div>
+                <Form.Check
+                    type={descriptor.type}
+                    label={descriptor.label}
+                    id={descriptor.field}
+                    checked={value}
+                    onChange={onChange}
+                />
+                <div className="text-danger mt-0 mb-2">{descriptor.error}</div>
+            </div>
+        );
+    }
 
     if (descriptor.base_type === 'varchar') {
         return (
             <div>
-                <label htmlFor={descriptor.field}
-                    className="form-label mt-3">{descriptor.label + ': '}</label>
-                <div className="input-group mb-4">
-                    <span className="input-group-text" >
-                        <i className={descriptor.icon}></i>
-                    </span>
-                    <input type="text"
-                        className="form-control"
+                <FloatingLabel
+                    label={descriptor.label}
+                    className="mb-3"
+                >
+                    <Form.Control type={descriptor.type}
+                        title={descriptor.title}
                         id={descriptor.field}
                         placeholder={descriptor.placeholder}
                         onChange={onChange}
                         value={value} />
-                    <span className="input-group-text">
-                        <span className="tt"
-                            data-bs-placement="bottom"
-                            title={descriptor.title}>
-                            <i className="bi bi-question-circle"></i>
-                        </span>
-                    </span>
-                </div>
+                </FloatingLabel>
                 <div className="text-danger mt-0 mb-2">{descriptor.error}</div>
             </div>
         );
