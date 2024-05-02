ed17e05984c9f0b83758846ed482fcbdcebdbd0a
flub78 <frederic.peignot>
2024-05-02 16:35:21 +0200

    Work on React code generation

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -97,9 +97,11 @@ const BoardList = () => {
                 <td> <Cell value="{board.email}" subtype="email" > </Cell></td>
                 <td> <Cell value="{board.favorite}" subtype="boolean"></Cell></td>
                 <td> <Cell value="{board.href}" subtype="string" > </Cell></td>
-                <td> <Cell value="{board.image}" subtype="string" > </Cell></td>
-                <td> <Cell value="{board.theme}" subtype="string" > </Cell></td>
-                <td> <Cell value="{board.lists}" subtype="string" > </Cell></td>
+			<td> <Cell value="{board.image}" subtype="image" > </Cell></td>
+			<td> <Cell value="{board.theme}" subtype="enum" > </Cell></td>
+			<td> <Cell value="{board.lists}" subtype="csv_string" > </Cell></td>
+
+
             </tr >
         );
     });
