4705b39080ab6e2566543fabacf2ab0538bdb4dc
flub78 <frederic.peignot>
2024-06-12 10:26:58 +0200

    Some work on the login page

---
+++ doc/react_login.md
@@ -0,0 +1,28 @@
+# React Login
+
+As the REST API uses token for authentication, the client must manage this authentication.
+
+* fetch a token on login
+* use this token when making a request
+* release the token on logout.
+
+
+## 
+
+Login form
+
+https://www.youtube.com/watch?v=R6HQ1u0hSJo&list=PLwJWw4Pbl4w_HNb-5GbpQx6ai5OEEy9pI&index=3
+
+https://www.youtube.com/watch?v=K_YTd-X1uTw&list=PLwJWw4Pbl4w_HNb-5GbpQx6ai5OEEy9pI&index=15
+
+
+
+## React application structure
+
+* One public area
+  * login
+  * forgotten password
+  * contact page
+  * public information
+   
+* One private area for logged in users
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -11,7 +11,7 @@ import LoginForm from '../components/LoginForm';
 
 const LoginPage = () => {
 
-    const { t } = useTranslation(['translation', 'boards']);
+    const { t } = useTranslation(['translation', 'login']);
 
     const [boardsData, setBoardsData] = React.useState([]);
 
@@ -28,29 +28,35 @@ const LoginPage = () => {
     }, []);
 
     return (
-        <div className="container d-flex flex-column align-items-center w-50">
+        <div>
+            <Navbar theme="light" boardsData={boardsData} />
 
-            <h3 className="text-center m-5" >Frello</h3>
 
-            <p>Welcome</p>
+            <div className="container d-flex flex-column align-items-center w-50">
 
-            <div className="container d-flex mt-5 justify-content-center">
+                <h3 className="text-center m-5" >Frello</h3>
 
-                <Card className="">
-                    <Card.Header className="card-header">
-                        <h3>Login</h3>
-                    </Card.Header>
+                <p>{t("login:welcome")}</p>
 
-                    <Card.Body>
-                        <div>
-                            <LoginForm />
-                        </div>
-                    </Card.Body>
-                </Card>
+                <div className="container d-flex mt-5 justify-content-center">
+
+                    <Card className="">
+                        <Card.Header className="card-header">
+                            <h3>{t("login:login")}</h3>
+                        </Card.Header>
+
+                        <Card.Body>
+                            <div>
+                                <LoginForm />
+                            </div>
+                        </Card.Body>
+                    </Card>
+
+                </div>
 
             </div>
-
         </div>
+
     );
 };
 
